8614d9b19c4df83f0feb40ba4e7c16428bc5bbf9
Roberto <robhhr@outlook.com>
2019-07-08 23:25:45 -0700

    style journal posts and button for reading

--- themes/inhabitant/template-parts/content.php
+++ themes/inhabitant/template-parts/content.php
@@ -13,7 +13,7 @@
 			<?php the_post_thumbnail( 'large' ); ?>
 		<?php endif; ?>
 
-		<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
+		<?php the_title( sprintf( '<h2 class="entry-title"><div class="int-text"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></div></h2>' ); ?>
 
 		<?php if ( 'post' === get_post_type() ) : ?>
 		<div class="entry-meta">
@@ -25,4 +25,8 @@
 	<div class="entry-content">
 		<?php the_excerpt(); ?>
 	</div><!-- .entry-content -->
+
+	<div class="read-more">
+		<a href="#" class="button">Read More &rarr;</a>
+	</div>
 </article><!-- #post-## -->
