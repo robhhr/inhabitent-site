d3279ae7c0846238cabb8628cd398f65ad317cf1
Roberto <robhhr@outlook.com>
2019-07-11 01:15:09 -0700

    add & style products layout

--- themes/inhabitant/archive-product.php
+++ themes/inhabitant/archive-product.php
@@ -20,9 +20,10 @@ get_header(); ?>
 		<main class="product-container">
 
 		<?php 
-		// the query
+		// Product loop
 		$the_query = new WP_Query( array(
 			'post_type' => 'product',
+			'order' => 'ASC',
 			'posts_per_page' => 16,
 		)); 
 		?>
@@ -31,16 +32,15 @@ get_header(); ?>
 		<?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
 			
 			<?php get_template_part( 'template-parts/content-product' ); ?>
-			<?php the_title(); ?>
+			
 
 		<?php endwhile; ?>
 		<?php wp_reset_postdata(); ?>
 
 		<?php else : ?>
-		<p><?php __('No News'); ?></p>
+		<p><?php __('No product found'); ?></p>
 		<?php endif; ?>
-
-				
+		<!-- End loop -->
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
--- themes/inhabitant/template-parts/content-product.php
+++ themes/inhabitant/template-parts/content-product.php
@@ -7,23 +7,9 @@
 
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<!-- <header class="product-header"> -->
-		<?php if ( has_post_thumbnail() ) : ?>
-			<?php the_post_thumbnail( 'large' ); ?>
-		<?php endif; ?>
-
-		<?php the_title( sprintf( '<h2 class="product-title"><div class="product-text"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></div></h2>' ); ?>
-
-		<?php if ( 'post' === get_post_type() ) : ?>
-		<div class="entry-meta">
-			<?php red_starter_posted_on(); ?> / <?php comments_number( '0 Comments', '1 Comment', '% Comments' ); ?> / <?php red_starter_posted_by(); ?>
-		</div><!-- .entry-meta -->
-		<?php endif; ?>
-	<!-- .entry-header -->
-
-	<div class="entry-content">
-		<?php the_excerpt(); ?>
-	</div><!-- .entry-content -->
-
-</article><!-- #post-## -->
+<div class="product-area" id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<?php if ( has_post_thumbnail() ) : ?>
+	<?php the_post_thumbnail( 'full' ); ?>
+	<?php endif; ?>
+	<?php the_title( sprintf( '<div class="product-text"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></div>' ); ?>
+</div>
