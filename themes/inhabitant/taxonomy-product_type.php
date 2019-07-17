74569954063f0e82603b97d3c6dae77ad4567d59
Roberto <robhhr@outlook.com>
2019-07-16 23:53:25 -0700

    style taxonomy/type content

--- themes/inhabitant/taxonomy-product_type.php
+++ themes/inhabitant/taxonomy-product_type.php
@@ -7,29 +7,24 @@
 
 get_header(); ?>
 
-<div class="shop-container">
-	<div class="shop-title">
-		<h1><?php single_term_title(); ?></h1>
+<div class="taxonomy">
+	<div class="tax-container">
+		<div class="shop-title">
+			<h1><?php single_term_title(); ?></h1>
+		</div>
+		<div class="tax-nav">
+			<?php the_archive_description(); ?>
+		</div>
 	</div>
-</div>
-
-<?php the_archive_description(); ?>
 
 	<div id="primary" class="content-taxonomy">
-		
+
 		<?php while ( have_posts() ) : the_post(); ?>
 
-			<?php get_template_part( 'template-parts/content-product', 'single' ); ?>
-
-			<?php
-				// If comments are open or we have at least one comment, load up the comment template.
-				if ( comments_open() || get_comments_number() ) :
-					comments_template();
-				endif;
-			?>
+		<?php get_template_part( 'template-parts/content-product', 'single' ); ?>
 
 		<?php endwhile; // End of the loop. ?>
 
 	</div><!-- #primary -->
-
+</div>
 <?php get_footer(); ?>
