14b18b6f39f74044f00283d0eade5ced84741193
Roberto <robhhr@outlook.com>
2019-07-18 15:03:31 -0700

    add journal section home-page

--- themes/inhabitant/front-page.php
+++ themes/inhabitant/front-page.php
@@ -6,17 +6,20 @@
  */
 
 get_header('home'); ?>
-
-		<main id="main" class="site-main-alt" role="main">
+<!-- Header -->
+		<main id="main" class="site-main-fp" role="main">
 			<?php while ( have_posts() ) : the_post(); ?>
 				<div class="image-hero">
-					<?php echo '<style type="text/css"> .image-hero { background-image:url('.get_stylesheet_directory_uri().'/images/inhabitent-logo-full.svg) !important; height: 250px !important; width: 250px !important; background-size: 100% !important; background-repeat: no-repeat; position: absolute; z-index: 100; top: 40%; left: 40%;}</style>';?>
+					<?php echo '<style type="text/css"> .image-hero { background-image:url('.get_stylesheet_directory_uri().'/images/inhabitent-logo-full.svg) !important; height: 250px !important; width: 250px !important; background-size: 100% !important; background-repeat: no-repeat; position: absolute; z-index: 100; top: 40%; left: 42%;}</style>';?>
 				</div>
 				<?php the_content(); ?>
 			<?php endwhile; ?>
 		</main>
 
-		<!-- Posts Loop -->
+<!-- Posts Loop -->
+<div class="blog-front">
+	<h1>Inhabitent Journal</h1>
+	<div class="journal-front">
 		<?php
 		$shop_loop = new WP_Query( array(
 			'post_type' => 'post',
@@ -28,7 +31,7 @@ get_header('home'); ?>
 		<?php if ( $shop_loop->have_posts() ) : ?>
 		<?php while ( $shop_loop->have_posts() ) : $shop_loop->the_post(); ?>
 			
-			<?php get_template_part( 'template-parts/content' ); ?>			
+			<?php get_template_part( 'template-parts/content-posts-fp' ); ?>			
 
 		<?php endwhile; ?>
 		<?php wp_reset_postdata(); ?>
@@ -36,7 +39,9 @@ get_header('home'); ?>
 		<?php else : ?>
 		<p><?php __('No post found'); ?></p>
 		<?php endif; ?>
-		<!-- End loop -->
+	</div>
+</div>
+<!-- End loop -->
 
 
 <?php get_footer(); ?>
---
+++ themes/inhabitant/template-parts/content-posts-fp.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Template part for displaying posts.
+ *
+ * @package RED_Starter_Theme
+ */
+
+?>
+
+<div class="product-area" id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+        <?php if ( 'post' === get_post_type() ) : ?>
+        <?php endif; ?>
+        <?php if ( has_post_thumbnail() ) : ?>
+            <?php the_post_thumbnail( 'large' ); ?>
+        <?php endif; ?>
+    <div class="product-text-fp">
+        <div class="entry-meta">
+            <?php red_starter_posted_on(); ?> / <?php comments_number( '0 Comments', '1 Comment', '% Comments' ); ?> / <?php red_starter_posted_by(); ?>
+        </div>
+        <div class="entry-content-fp">
+            <?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
+        </div>
+            <?php echo sprintf( '<div class="read-entry"><a href="%s" rel="bookmark"><div class="button">', esc_url( get_permalink() )).'Read Entry</div></a></div>'; ?>
+    </div>
+</div>
