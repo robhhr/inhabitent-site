413f6f43e6864038c591127cf18357f409fd4d78
Roberto <robhhr@outlook.com>
2019-07-17 23:03:25 -0700

    add about template w/ hero image

--- themes/inhabitant/page-about.php
+++ themes/inhabitant/page-about.php
@@ -5,17 +5,16 @@
  * @package RED_Starter_Theme
  */
 
-get_header(); ?>
+get_header('about'); ?>
 
-	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
 			<?php while ( have_posts() ) : the_post(); ?>
-				<?php the_content(); ?>
+
+			<?php the_content(); ?>
 
 			<?php endwhile; // End of the loop. ?>
 
 		</main><!-- #main -->
-	</div><!-- #primary -->
 
 <?php get_footer(); ?>
