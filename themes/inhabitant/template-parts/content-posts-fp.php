09e60c829e9c0add3ee5623ad989825416b77ec0
Roberto <robhhr@outlook.com>
2019-07-19 02:00:45 -0700

    style journal home-page

--- themes/inhabitant/template-parts/content-posts-fp.php
+++ themes/inhabitant/template-parts/content-posts-fp.php
@@ -14,8 +14,8 @@
             <?php the_post_thumbnail( 'large' ); ?>
         <?php endif; ?>
     <div class="product-text-fp">
-        <div class="entry-meta">
-            <?php red_starter_posted_on(); ?> / <?php comments_number( '0 Comments', '1 Comment', '% Comments' ); ?> / <?php red_starter_posted_by(); ?>
+        <div class="entry-meta-fp">
+            <?php red_starter_posted_on(); ?> / <?php comments_number( '0 Comments', '1 Comment', '% Comments' ); ?>
         </div>
         <div class="entry-content-fp">
             <?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
