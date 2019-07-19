40b4b86b8ca8447aee0aeab5d5abb3548b49d8bf
Roberto <robhhr@outlook.com>
2019-07-19 01:09:41 -0700

    style main adventures template

--- themes/inhabitant/archive-adventures.php
+++ themes/inhabitant/archive-adventures.php
@@ -22,6 +22,7 @@ get_header(); ?>
 					<?php the_title( sprintf( '<a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a>' ); ?>
 				<!-- </div> -->
 			</h2>
+			<?php echo sprintf( '<div class="read-more-ad"><a href="%s" rel="bookmark"><div class="button">', esc_url( get_permalink() )).'Read More</div></a></div>'; ?>
 		</div>
 	<?php endwhile; ?>
 </div>
