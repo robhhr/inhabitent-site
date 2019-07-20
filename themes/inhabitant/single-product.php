63b5104dfcef13850cb54da1148c5fb856501075
Roberto <robhhr@outlook.com>
2019-07-20 15:42:45 -0700

    add plugin for store schedule

--- themes/inhabitant/single-product.php
+++ themes/inhabitant/single-product.php
@@ -13,8 +13,6 @@ get_header(); ?>
 
 			<?php get_template_part( 'template-parts/content-single-product', 'single' ); ?>
 
-
-
 			<?php
 				// If comments are open or we have at least one comment, load up the comment template.
 				if ( comments_open() || get_comments_number() ) :
--- themes/inhabitant/single.php
+++ themes/inhabitant/single.php
@@ -13,9 +13,11 @@ get_header(); ?>
 		<?php while ( have_posts() ) : the_post(); ?>
 
 			<?php get_template_part( 'template-parts/content-single' ); ?>
-
-			<?php the_post_navigation(); ?>
-
+				<div class="social-buttons-blog">
+					<?php echo sprintf( '<div class="social-more"><a href="%s" rel="bookmark"><div class="button-blog">', esc_url( get_permalink() )).'<i class="fa fa-facebook-f"></i>Like</div></a></div>'; ?>
+					<?php echo sprintf( '<div class="social-more"><a href="%s" rel="bookmark"><div class="button-blog">', esc_url( get_permalink() )).'<i class="fa fa-twitter"></i>Tweet</div></a></div>'; ?>
+					<?php echo sprintf( '<div class="social-more"><a href="%s" rel="bookmark"><div class="button-blog">', esc_url( get_permalink() )).'<i class="fa fa-pinterest"></i>Pin</div></a></div>'; ?>
+				</div>
 			<?php
 				// If comments are open or we have at least one comment, load up the comment template.
 				if ( comments_open() || get_comments_number() ) :
